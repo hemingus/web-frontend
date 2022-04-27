d7b11a75db2b6564685725677814076985e3b3aa
hemingus <54212159+hemingus@users.noreply.github.com>
2022-04-27 15:21:01 +0200

    🔨 refactor ccs files to each component (#5)
    * 🔨css for header in new Header.css
    
    * 🔨moved css in new Footer.css
    
    * 🔨moved css to new ShufflingImage.css

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -7,21 +7,19 @@ import ShufflingImage from './components/imageComponents/ShufflingImage';
 function App() {
   return (
     <div className="App"> 
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
       <ShufflingImage imageList={["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"]} />
       <Footer />
     </div>
