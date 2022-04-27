73b18f5e797651bc8cd154831878b0a2ba151eb5
hemingus <54212159+hemingus@users.noreply.github.com>
2022-04-27 16:22:59 +0200

    🔥 removed react template stuff (#6)

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -8,18 +8,6 @@ function App() {
   return (
     <div className="App"> 
       <Header />
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
       <ShufflingImage imageList={["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"]} />
       <Footer />
     </div>
