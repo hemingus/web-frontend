f3b0755ba1fa11bc9f91064c9079fbdc877f7f56
hemingus <54212159+hemingus@users.noreply.github.com>
2022-04-29 13:55:23 +0200

    ✨Feature/embed video (#7)
    * ✨ new component YoutubeEmbed
    
    * 💄tweaked YoutubeEmbed.css

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import YoutubeEmbed from './components/YoutubeEmbed/YoutubeEmbed';
 import ShufflingImage from './components/imageComponents/ShufflingImage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App"> 
       <Header />
       <ShufflingImage imageList={["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"]} />
+      <YoutubeEmbed embedId="HTXG2Xy0dsU" />
       <Footer />
     </div>
   );
