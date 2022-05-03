60a1265baa739221ad9dec5ce1e6b9852a4354aa
hemingus <54212159+hemingus@users.noreply.github.com>
2022-05-03 15:00:42 +0200

    ✨Feature/video container (#8)
    * ✨ added video container component
    
    * ✨component video-container

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -4,13 +4,18 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import YoutubeEmbed from './components/YoutubeEmbed/YoutubeEmbed';
 import ShufflingImage from './components/imageComponents/ShufflingImage';
+import VideoContainer from './components/VideoContainer/VideoContainer';
+
+const embedIds = ["HTXG2Xy0dsU", "HTXG2Xy0dsU", "HTXG2Xy0dsU", "HTXG2Xy0dsU", "HTXG2Xy0dsU", "HTXG2Xy0dsU"];
+const images = ["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"];
 
 function App() {
   return (
     <div className="App"> 
       <Header />
-      <ShufflingImage imageList={["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"]} />
+      <ShufflingImage imageList={images} />
       <YoutubeEmbed embedId="HTXG2Xy0dsU" />
+      <VideoContainer embedIds={embedIds} />
       <Footer />
     </div>
   );
