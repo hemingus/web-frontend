6aac6d2817f7d79cdf0d99959ae881620e2ced87
hemingus <heming.hanevik@gmail.com>
2022-06-07 18:25:23 +0200

    ✨ added Comment Field

--- web-frontend/src/components/CommentField/CommentField.js
+++ web-frontend/src/components/CommentField/CommentField.js
@@ -1,22 +1,35 @@
 import React from "react"
 import { useState } from "react"
 import "./CommentField.css"
+import dateToString from "../../utilities/DateTimeUtilities"
 
 const CommentField = () => {
     function handleSubmit(event) {
         event.preventDefault()
-        setComments(prev => [document.getElementById("comment").value, ...prev])
+        setComments(prev => {
+            const newComment = {
+                name: document.getElementById("name").value,
+                timestamp: Date.now(),
+                comment: document.getElementById("comment").value
+            }
+            return [newComment, ...prev]
+        })
     }
+    
     const [comments, setComments] = useState([])
     return (
         <>
         <form onSubmit={handleSubmit}>
             <h3 className="CommentHeader">Leave comment</h3>
-            <input className="CommentField" type="text" id="comment"></input>
-            <button className="CommentSubmit">Submit</button>
+            <input className="CommentField" type="text" id="name" placeholder="Your name..."></input>
+            <input className="CommentField" type="text" id="comment" placeholder="Your comment..."></input>
+            <button className="CommentSubmit" submit-tooltip="Great!">Submit</button>
         </form>
         <div>
-            <ul>{comments.map(comment => <li>{comment}</li>)}</ul>
+            <ul className="CommentList">{comments.map(comment => <li className="CommentObjects">
+                <h5 className="Names">{dateToString(comment.timestamp) + " " + comment.name + " wrote: "}</h5>
+                <p className="Comments">{comment.comment}</p>
+            </li>)}</ul>
         </div>
         </>
     )
