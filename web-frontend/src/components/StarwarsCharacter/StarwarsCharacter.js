44c9ade67b55fc9a6af280d349f6f7c66cd1b05d
hemingus <54212159+hemingus@users.noreply.github.com>
2022-07-05 19:09:53 +0200

    Expansion/starwars chars (#19)
    * 🔨💄 expanded on starwars characters
    
    * 💄aligned text left
    
    * 🔥removed unused code lines
    
    Co-authored-by: Heming Hanevik <Heming.Hanevik@student.uib.no>

--- web-frontend/src/components/StarwarsCharacter/StarwarsCharacter.js
+++ web-frontend/src/components/StarwarsCharacter/StarwarsCharacter.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import "./StarwarsCharacter.css"
 
-const StarwarsCharacter = ({name, height, mass}) => {
-    
+const StarwarsCharacter = ({name, height, mass, birth_year, eye_color, gender, hair_color, skin_color}) => {
+
     return (
         <div className="char">
-            <ul className="starwarsCharacter">
+        
+            <ul className="nameplate">
                 <li>Name: {name}</li>
                 <li>Height: {height}</li>
                 <li>Mass: {mass}</li>
             </ul>
+        
+            <ul className="attributes">
+                <li>Birth year: {birth_year}</li>
+                <li>Eye color: {eye_color}</li>
+                <li>Gender: {gender}</li>
+                <li>Hair color: {hair_color}</li>
+                <li>Skin color: {skin_color}</li>
+            </ul>
+
         </div>
     )
 } 
