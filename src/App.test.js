dadfcc344c057ed5044d5914b1755de37099daa7
hemingus <heming.hanevik@gmail.com>
2022-07-07 15:52:15 +0200

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function sum(a, b) {
+  return a + b;
+}
+
+test('adds 1+2 = 3', () => {
+  expext(sum(1,2)).toBe(3);
 });
