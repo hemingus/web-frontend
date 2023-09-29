44b77a7cae26eef3fdbac5a81bc37ff4577d71c9
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-09-29 10:48:48 +0200

    ✨🔨 CommentField now communicates with API

--- src/components/CommentField/CommentField.js
+++ src/components/CommentField/CommentField.js
@@ -1,22 +1,41 @@
-import React from "react"
-import { useState } from "react"
+import React from 'react'
+import axios from 'axios'
 import "./CommentField.css"
-import dateToString from "../../utilities/DateTimeUtilities"
+import {useState, useEffect} from 'react'
 
 const CommentField = () => {
-    function handleSubmit(event) {
-        event.preventDefault()
-        setComments(prev => {
-            const newComment = {
-                name: document.getElementById("name").value,
-                timestamp: Date.now(),
-                comment: document.getElementById("comment").value
-            }
-            return [newComment, ...prev]
-        })
-    }
-    
+
     const [comments, setComments] = useState([])
+
+    const getComments = async (url = "https://hemingmusicapi.azurewebsites.net/Comment", data = []) => {
+        try {
+            const res = await axios.get(url)
+            setComments(res.data)
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+
+    useEffect(() => {
+        getComments();
+    }, [])
+
+    const handleSubmit = async (event, url = "https://hemingmusicapi.azurewebsites.net/Comment") => {
+        event.preventDefault()
+        const newComment = {
+            name: document.getElementById("name").value,
+            commentBody: document.getElementById("comment").value
+        }
+        try {
+            await axios.post(url, newComment)
+            getComments()
+        }
+        catch (err) {
+            alert(err.message)
+        }
+    }
+
     return (
         <>
         <form onSubmit={handleSubmit}>
@@ -26,9 +45,10 @@ const CommentField = () => {
             <button className="CommentSubmit" submit-tooltip="Great!">Submit</button>
         </form>
         <div>
+            <h1 className="CommentHeader">Comments</h1>
             <ul className="CommentList">{comments.map(comment => <li className="CommentObjects">
-                <h5 className="Names">{dateToString(comment.timestamp) + " " + comment.name + " wrote: "}</h5>
-                <p className="Comments">{comment.comment}</p>
+                <h5 className="Names">{comment.name + " wrote: "}</h5>
+                <p className="Comments">{comment.commentBody}</p>
             </li>)}</ul>
         </div>
         </>
