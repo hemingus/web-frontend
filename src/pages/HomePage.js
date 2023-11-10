cbe76637663bc533de3e60a1f128851ab554f8ee
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-11-10 16:23:24 +0100

    removed redundancy

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../components/Header/Header';
-import InteractiveLogo from '../components/InteractiveLogo/InteractiveLogo';
 import MainMenu from '../components/MainMenu/MainMenu';
 import ShufflingImage from '../components/imageComponents/ShufflingImage';
 import CommentField from '../components/CommentField/CommentField';
