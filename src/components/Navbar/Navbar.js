65ad59679011a0749e309095e4829775c85873c9
hemingus <54212159+hemingus@users.noreply.github.com>
2023-12-02 00:22:47 +0100

    Responsive design (#30)
    * Responsive navbar
    
    * Revert navbar
    
    ---------
    
    Co-authored-by: Heming Hanevik <Heming.Hanevik@student.uib.no>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./Navbar.css"
-import { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { CustomLink } from '../../utilities/CustomLink'
 import Dropdown from '../DropdownMenu/Dropdown'
@@ -32,39 +31,19 @@ export default function Navbar() {
       "title": "Log in",
       "route": "/login"}
     ]
-
-    const lines = '\u2630'
-    const navRef = useRef(null)
-    const [btnText, setBtnText] = useState(lines)
-
-    const handleClick = () => {
-        showNavBar()
-        setBtnText((prevText) =>
-          prevText === lines ? "Close" : lines
-        );
-      };
-
-    const showNavBar = () => {
-        navRef.current?.classList.toggle("responsive_nav")
-    }
-
-
-      
+    
     return ( 
-      <div className="nav">
-        <nav  ref={navRef}>
-          <Link to="/" className="site-title" style={{color: 'greenyellow'}}>
-            Hemings Music Space
-          </Link>
-          <ul>
-            <Dropdown title="User" elements={RegisterDropdownProps}/>
-            <Dropdown title="Videos" elements={VideoDropdownProps}/>
-            <CustomLink to="/animation">3D Animation</CustomLink>
-            <Dropdown title="Starwars API" elements={StarwarsDropdownProps}/>
-            <button onClick={handleClick}>{btnText}</button>
-          </ul>
-        </nav>
-      </div>
+      <nav className="nav">
+        <Link to="/" className="site-title" style={{color: 'greenyellow'}}>
+          Hemings Music Space
+        </Link>
+        <ul>
+          <Dropdown title="User" elements={RegisterDropdownProps}/>
+          <Dropdown title="Videos" elements={VideoDropdownProps}/>
+          <CustomLink to="/animation">3D Animation</CustomLink>
+          <Dropdown title="Starwars API" elements={StarwarsDropdownProps}/>
+        </ul>
+      </nav>
     )      
 }
 
