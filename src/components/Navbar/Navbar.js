3b2cd7ba192443de5433407026dcb5d76e614b08
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-12-01 23:53:54 +0100

    Responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Navbar.css"
+import { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { CustomLink } from '../../utilities/CustomLink'
 import Dropdown from '../DropdownMenu/Dropdown'
@@ -32,21 +33,38 @@ export default function Navbar() {
       "route": "/login"}
     ]
 
+    const lines = '\u2630'
+    const navRef = useRef(null)
+    const [btnText, setBtnText] = useState(lines)
+
+    const handleClick = () => {
+        showNavBar()
+        setBtnText((prevText) =>
+          prevText === lines ? "Close" : lines
+        );
+      };
+
+    const showNavBar = () => {
+        navRef.current?.classList.toggle("responsive_nav")
+    }
 
 
       
     return ( 
-      <nav className="nav">
-        <Link to="/" className="site-title" style={{color: 'greenyellow'}}>
-          Hemings Music Space
-        </Link>
-        <ul>
-          <Dropdown title="User" elements={RegisterDropdownProps}/>
-          <Dropdown title="Videos" elements={VideoDropdownProps}/>
-          <CustomLink to="/animation">3D Animation</CustomLink>
-          <Dropdown title="Starwars API" elements={StarwarsDropdownProps}/>
-        </ul>
-      </nav>
+      <div className="nav">
+        <nav  ref={navRef}>
+          <Link to="/" className="site-title" style={{color: 'greenyellow'}}>
+            Hemings Music Space
+          </Link>
+          <ul>
+            <Dropdown title="User" elements={RegisterDropdownProps}/>
+            <Dropdown title="Videos" elements={VideoDropdownProps}/>
+            <CustomLink to="/animation">3D Animation</CustomLink>
+            <Dropdown title="Starwars API" elements={StarwarsDropdownProps}/>
+            <button onClick={handleClick}>{btnText}</button>
+          </ul>
+        </nav>
+      </div>
     )      
 }
 
