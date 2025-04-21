4a2912752ca016564d7e7e9a780985def45fd667
Heming Hanevik <heming.hanevik@gmail.com>
2025-04-21 22:37:17 +0200

    added alt prop to img

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ const images = ["gulkins1.jpg", "gulkins2.jpg", "hemingdrums.jpg"];
 export default function HomePage() {
     return (
         <div className="HomePage">
-            <img src="onkel_heming_trafikkskole.png" alt="picture: onkel heming trafikkskole"/>
+            <img src="onkel_heming_trafikkskole.png" alt="onkel_heming_trafikkskole.png"/>
             <Header />
             <MainMenu />
             <GuitarTuner />
